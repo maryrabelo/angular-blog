92cebd734b2b03ed41fa93c61928a5cd717f9b14
maryrabelo <mariany.rabelo.mr#gmail.com>
2022-11-30 18:58:59 -0300

    components dinamycs properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.scss']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6fJcPjivBXyvm1VAXs8uCeD-LGlbtoFfeAA&usqp=CAU"
+  cardTitle:string= "LOOK DO DIA: MIX DE PEÇAS"
+  cardDescription:string =" Bom dia meus amores! Semana bem relax por aqui! Muito delicioso um lookinho fresh né? Principalmente quando as peças são super descoladas."
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.scss']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string ="https://viagemeturismo.abril.com.br/wp-content/uploads/2016/11/thinkstockphotos-4549879531.jpeg"
+  
+  @Input()
+  cardTitle:string = "MARY EM PARIS: DICAS E INSPIRAÇÕES"
+  constructor() { }
+
+  ngOnInit(): void {
+    }
 }
